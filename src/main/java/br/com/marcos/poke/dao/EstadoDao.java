16655a7e5ba5268a7c166b24e7f2270fcc2102d4
Marcos Avila Isidoro <marcavis@yahoo.com.br>
2017-06-08 01:18:23 -0300

    não precisamos mais de estados e cidades

--- src/main/java/br/com/marcos/poke/dao/EstadoDao.java
+++
@@ -1,7 +0,0 @@
-package br.com.marcos.poke.dao;
-
-import br.com.marcos.poke.domain.Estado;
-
-public class EstadoDao extends GenericDao<Estado> {
-
-}
--- src/main/java/br/com/marcos/poke/main/HibernateUtilTest.java
+++ src/main/java/br/com/marcos/poke/main/HibernateUtilTest.java
@@ -2,20 +2,11 @@ package br.com.marcos.poke.main;
 
 import java.util.ArrayList;
 
-import br.com.marcos.poke.dao.EstadoDao;
-import br.com.marcos.poke.domain.Estado;
-
 public class HibernateUtilTest {
 	public static void main(String[] args) {
 		//Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
 		//sessao.close();
 		//HibernateUtil.getFabricaDeSessoes().close();
-		
-		EstadoDao estado = new EstadoDao();
-		ArrayList<Estado> lista = (ArrayList<Estado>)estado.listarTodos();
-		for (Estado e : lista) {
-			System.out.println(e.getNome()+"-"+e.getSigla());
-		}
 		
 		//GenericDao<Estado> dao = new EstadoDao();
 		//dao.Salvar(e);
