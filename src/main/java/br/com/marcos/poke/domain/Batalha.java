9e65aff83ad71c43b1b92215f58dbb21edbfd94d
Marcos Avila Isidoro <marcavis@yahoo.com.br>
2017-06-12 00:56:17 -0300

    trabalhando na batalha pra usar especimes

--- src/main/java/br/com/marcos/poke/domain/Batalha.java
+++ src/main/java/br/com/marcos/poke/domain/Batalha.java
@@ -4,56 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Batalha{
-	private Pokemon pokemon1;
-	private Pokemon pokemon2;
+	private Especime pokemon1;
+	private Especime pokemon2;
 	private List<String> mensagens;
 	
 	public void lutar() {
 		mensagens = new ArrayList<String>();
-		int[] vida = {pokemon1.getVida(), pokemon2.getVida()};
+		int[] vida = {pokemon1.getPokemon().getVida(), pokemon2.getPokemon().getVida()};
 		//mensagens.add(String.valueOf(pokemon1.getVida()));
 		//mensagens.add(String.valueOf(pokemon2.getVida()));
 		int dano = 0;
 		while(vida[0] > 0 && vida[1] > 0) {
-			mensagens.add(pokemon1.getNome() + " tem " + vida[0] + " pontos de vida.");
-			mensagens.add(pokemon2.getNome() + " tem " + vida[1] + " pontos de vida.");
+			mensagens.add(pokemon1.getApelidoOuNome() + " tem " + vida[0] + " pontos de vida.");
+			mensagens.add(pokemon2.getApelidoOuNome() + " tem " + vida[1] + " pontos de vida.");
 			dano = calculaDano(pokemon1, pokemon2);
-			mensagens.add(pokemon1.getNome() + " ataca " + pokemon2.getNome() + ", causando "
+			mensagens.add(pokemon1.getPokemon().getNome() + " ataca " + pokemon2.getApelidoOuNome() + ", causando "
 					+ dano + " pontos de dano!");
 			vida[1] -= dano;
 			if(vida[1] < 1) {
-				mensagens.add(pokemon2.getNome() + " não consegue mais lutar!");
-				mensagens.add(pokemon1.getNome() + " é o vencedor!");
+				mensagens.add(pokemon2.getApelidoOuNome() + " não consegue mais lutar!");
+				mensagens.add(pokemon1.getApelidoOuNome() + " é o vencedor!");
 				return;
 			}
 			dano = calculaDano(pokemon2, pokemon1);
-			mensagens.add(pokemon2.getNome() + " ataca " + pokemon1.getNome() + ", causando "
+			mensagens.add(pokemon2.getApelidoOuNome() + " ataca " + pokemon1.getApelidoOuNome() + ", causando "
 					+ dano + " pontos de dano!");
 			vida[0] -= dano;
 			if(vida[0] < 1) {
-				mensagens.add(pokemon1.getNome() + " não consegue mais lutar!");
-				mensagens.add(pokemon2.getNome() + " é o vencedor!");
+				mensagens.add(pokemon1.getApelidoOuNome() + " não consegue mais lutar!");
+				mensagens.add(pokemon2.getApelidoOuNome() + " é o vencedor!");
 			}
 		}
 	}
 	
-	public int calculaDano (Pokemon p1, Pokemon p2) {
-		return Math.max(2, p1.getAtaque()-p2.getDefesa());
+	public int calculaDano (Especime p1, Especime p2) {
+		return Math.max(2, p1.getPokemon().getAtaque()-p2.getPokemon().getDefesa());
 	}
 	
-	public Pokemon getPokemon1() {
+	public Especime getPokemon1() {
 		return pokemon1;
 	}
 	
-	public void setPokemon1(Pokemon pokemon1) {
+	public void setPokemon1(Especime pokemon1) {
 		this.pokemon1 = pokemon1;
 	}
 	
-	public Pokemon getPokemon2() {
+	public Especime getPokemon2() {
 		return pokemon2;
 	}
 	
-	public void setPokemon2(Pokemon pokemon2) {
+	public void setPokemon2(Especime pokemon2) {
 		this.pokemon2 = pokemon2;
 	}
 
